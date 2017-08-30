409b014aff80d08eb9f747bbf7581be61ad39d03
Tyler Jurecki <jurecki07@yahoo.com>
2017-08-30 11:28:47 -0500

    sqlizing into db

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const app = express();
 const models = require("./models");
 const port = process.env.PORT || 3000;
 
+models.users.create({
+    name:'2ndname',
+    email:'2nd email',
+    bio:'writing other bio'
+
+}).then(function(newUser){
+    console.log("new user", newUser);
+})
+
 models.users.findOne().then(function (users) {
-    console.log(users);
+
+    console.log("found other user",users);
   });
 
 
